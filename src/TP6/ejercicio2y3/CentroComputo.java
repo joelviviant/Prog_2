7baaa6b0185c629cfc4b58a2084af4766e147a22
Joel Viviant <joelviviant31@gmail.com>
2024-02-16 12:08:52 -0300

    ejercicio 2

---
+++ src/TP6/ejercicio2y3/CentroComputo.java
@@ -0,0 +1,37 @@
+package TP6.ejercicio2y3;
+public class CentroComputo {
+    private ColaEspera computadoras;
+    private ColaEspera procesos;
+
+    public CentroComputo() {
+        this.computadoras = new ColaEspera();
+        this.procesos = new ColaEspera();
+    }
+    public void addComputadora(Computadora compu){
+        if (compu==null) return;
+        if (procesos.tieneElementos()){
+            Proceso proc = (Proceso) procesos.getSiguiente();
+            compu.ejecutarProceso(proc);
+        }
+        else computadoras.addElemento(compu);
+    }
+
+    public void addProceso(Proceso proc){
+        if (proc==null) return;
+        if (computadoras.tieneElementos()){
+            Computadora libre = (Computadora) computadoras.getSiguiente();
+            libre.ejecutarProceso(proc);
+        }
+        else {
+            procesos.addElemento(proc);
+        }
+    }
+
+    /*public Computadora getSiguienteCompu(){
+        return (Computadora) computadoras.getSiguiente();
+    }
+
+    public Proceso getSiguienteProceso(){
+        return (Proceso) procesos.getSiguiente();
+    }*/
+}
---
+++ src/TP6/ejercicio2y3/ElementoCola.java
@@ -0,0 +1,5 @@
+package TP6.ejercicio2y3;
+
+public interface ElementoCola {
+    boolean esMayor(ElementoCola otro);
+}
