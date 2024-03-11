7a30425aab9be26d40c8c280c21157d447eede93
joelviviant <joelviviant31@gmail.com>
2024-03-11 12:58:13 -0300

    PARCIAL

--- src/ParcialesFinales/parcialViajes/PaquetePreViaje.java
+++ src/ParcialesFinales/parcialViajes/PaquetePreViaje.java
@@ -2,17 +2,16 @@ package ParcialesFinales.parcialViajes;
 
 import java.time.LocalDate;
 
-public class PaquetePreViaje extends PaqueteGeneral{
+public class PaquetePreViaje extends Paquete{
     LocalDate inicio;
     LocalDate fin;
     double descuento;
 
-    public PaquetePreViaje(int id, int cantidadPersonas,LocalDate inicio, LocalDate fin, double descuento) {
-        super(id, cantidadPersonas);
+
+    public PaquetePreViaje(int id, int cantidadPersonas, String alojamiento, String destino, String origen, double costo,LocalDate inicio, LocalDate fin, double  descuento) {
+        super(id, cantidadPersonas, alojamiento, destino, origen, costo);
         this.inicio=inicio;
         this.fin=fin;
         this.descuento=descuento;
     }
-
-
 }
